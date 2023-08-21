e6c09bdc9bc012f863841a79fbdc865d963a0f4e
Alexander <alexander.l.lai@student.unsw.edu.au>
2023-08-21 20:35:57 +1000

    feat: Basic dashboard layout with ShadCN

---
+++ components/SearchBar.jsx
@@ -0,0 +1,13 @@
+import { Input } from "@/components/ui/input"
+
+export function SearchBar() {
+  return (
+    <div>
+      <Input
+        type="search"
+        placeholder="Search..."
+        className="md:w-[100px] lg:w-[300px]"
+      />
+    </div>
+  )
+}
