77efd60769bdd5abec489360f65aba8545fb38f7
Alexander <alexander.l.lai@student.unsw.edu.au>
2023-08-25 20:21:36 +1000

    feat: Added more to dashboard page Now shows the top 5 groupmates of a user, the average rating, about me and more. Still need to work on some more mock functionality

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,5 +1,9 @@
+"use client"
+
+import { useEffect, useState } from 'react'
 import { CalendarDateRangePicker } from '../components/CalendarDateRangePicker'
 import { Button } from '@/components/ui/button'
+import { Icons } from '@/components/ui/icons'
 import {
   Tabs,
   TabsContent,
@@ -13,40 +17,75 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card"
+import {
+  Avatar,
+  AvatarFallback,
+  AvatarImage,
+} from "@/components/ui/avatar"
 import { FullNav } from '../components/FullNav'
+import { getAverageRating, getGroupMates, getUserAbout, getUserDetails } from '../mockData'
+import { useRouter } from 'next/navigation'
 
 export default function Dashboard() {
+  const [userDetails, setUserDetails] = useState({});
+  const [aboutMe, setAboutMe] = useState("");
+  const [favGroupMates, setFavGroupMates] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [rating, setRating] = useState(0);
+  const router = useRouter();
+
+  useEffect(() => {
+    const email = sessionStorage.getItem("email")
+    const details = getUserDetails(email);
+    if (!email || !details) {
+      router.push("/");
+    } else {
+      setUserDetails(details);
+      const about = getUserAbout(email)
+      setAboutMe(about ? about : "");
+      const mates = getGroupMates(email);
+      setFavGroupMates(mates ? mates : []);
+      setRating(getAverageRating(email));
+      setLoading(false);
+    }
+  }, []);
+
+  const handleEdit = () => {
+  }
+
+  if (loading) {
+    return (
+      <>
+        <div className="hidden flex-col items-center md:flex w-full">
+          <div className="flex-1 space-y-4 items-center justify-between">
+            <Icons.pagespinner className="container  h-[400px] animate-spin text-primary" />
+          </div>
+        </div>
+      </>
+    )
+  }
+
   return (
     <>
       <div className="hidden flex-col md:flex">
         <FullNav />
         <div className="flex-1 space-y-4 p-8 pt-6">
           <div className="flex items-center justify-between space-y-2">
-            <h2 className="text-3xl font-bold tracking-tight">Dashboard</h2>
-            <div className="flex items-center space-x-2">
-              <CalendarDateRangePicker />
-              <Button>Download</Button>
-            </div>
+            <h2 className="text-3xl font-bold tracking-tight">Hi {userDetails.fname}!</h2>
           </div>
           <Tabs defaultValue="overview" className="space-y-4">
             <TabsList>
               <TabsTrigger value="overview">Overview</TabsTrigger>
-              <TabsTrigger value="analytics" disabled>
-                Analytics
-              </TabsTrigger>
-              <TabsTrigger value="reports" disabled>
-                Reports
-              </TabsTrigger>
-              <TabsTrigger value="notifications" disabled>
+              <TabsTrigger value="notifications">
                 Notifications
               </TabsTrigger>
             </TabsList>
             <TabsContent value="overview" className="space-y-4">
-              <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
+              <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3">
                 <Card>
                   <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
                     <CardTitle className="text-sm font-medium">
-                      Total Revenue
+                      Current Load
                     </CardTitle>
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
@@ -58,20 +97,23 @@ export default function Dashboard() {
                       strokeWidth="2"
                       className="h-4 w-4 text-muted-foreground"
                     >
-                      <path d="M12 2v20M17 5H9.5a3.5 3.5 0 0 0 0 7h5a3.5 3.5 0 0 1 0 7H6" />
+                      <path d="M22 12h-4l-3 9L9 3l-3 9H2" />
                     </svg>
                   </CardHeader>
                   <CardContent>
-                    <div className="text-2xl font-bold">$45,231.89</div>
+                    <div className="text-2xl font-bold">
+                      {/* We will get this information from how many courses they have joined */}
+                      3 Courses
+                    </div>
                     <p className="text-xs text-muted-foreground">
-                      +20.1% from last month
+                      23 Courses Enrolled All-Time
                     </p>
                   </CardContent>
                 </Card>
                 <Card>
                   <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
                     <CardTitle className="text-sm font-medium">
-                      Subscriptions
+                      Groups
                     </CardTitle>
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
@@ -89,15 +131,17 @@ export default function Dashboard() {
                     </svg>
                   </CardHeader>
                   <CardContent>
-                    <div className="text-2xl font-bold">+2350</div>
+                    <div className="text-2xl font-bold">6 Groups</div>
                     <p className="text-xs text-muted-foreground">
-                      +180.1% from last month
+                      20 Groups Joined All-Time
                     </p>
                   </CardContent>
                 </Card>
                 <Card>
                   <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-                    <CardTitle className="text-sm font-medium">Sales</CardTitle>
+                    <CardTitle className="text-sm font-medium">
+                      Overall Rating
+                    </CardTitle>
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
                       viewBox="0 0 24 24"
@@ -113,34 +157,9 @@ export default function Dashboard() {
                     </svg>
                   </CardHeader>
                   <CardContent>
-                    <div className="text-2xl font-bold">+12,234</div>
+                    <div className="text-2xl font-bold">{rating}</div>
                     <p className="text-xs text-muted-foreground">
-                      +19% from last month
-                    </p>
-                  </CardContent>
-                </Card>
-                <Card>
-                  <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-                    <CardTitle className="text-sm font-medium">
-                      Active Now
-                    </CardTitle>
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 24 24"
-                      fill="none"
-                      stroke="currentColor"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      className="h-4 w-4 text-muted-foreground"
-                    >
-                      <path d="M22 12h-4l-3 9L9 3l-3 9H2" />
-                    </svg>
-                  </CardHeader>
-                  <CardContent>
-                    <div className="text-2xl font-bold">+573</div>
-                    <p className="text-xs text-muted-foreground">
-                      +201 since last hour
+                      Group Partners Rated You 8.7 Last Term
                     </p>
                   </CardContent>
                 </Card>
@@ -148,16 +167,45 @@ export default function Dashboard() {
               <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-7">
                 <Card className="col-span-4">
                   <CardHeader>
-                    <CardTitle>Overview</CardTitle>
+                    <div className="flex items-center justify-between space-y-2">
+                      <CardTitle>About You</CardTitle>
+                      <Button onClick={handleEdit}>Edit</Button>
+                    </div>
                   </CardHeader>
+                  <CardContent>
+                    <div className="text-l">
+                      {aboutMe}
+                    </div>
+                  </CardContent>
                 </Card>
                 <Card className="col-span-3">
                   <CardHeader>
-                    <CardTitle>Recent Sales</CardTitle>
+                    <CardTitle>Favourite Groupmates</CardTitle>
                     <CardDescription>
-                      You made 265 sales this month.
+                      These are {userDetails.fname}'s top groupmates.
                     </CardDescription>
                   </CardHeader>
+                  <CardContent>
+                    <div className="space-y-8">
+                      {favGroupMates.map((mate) => {
+                        return(
+                          <div className="flex items-center" key={`groupmate-${mate.email}`}>
+                            <Avatar className="h-9 w-9">
+                              <AvatarImage src="/avatars/01.png" alt="Avatar" />
+                              <AvatarFallback>{mate.initials}</AvatarFallback>
+                            </Avatar>
+                            <div className="ml-4 space-y-1">
+                              <p className="text-sm font-medium leading-none">{mate.name}</p>
+                              <p className="text-sm text-muted-foreground">
+                                {mate.email}
+                              </p>
+                            </div>
+                            <div className="ml-auto font-medium">{mate.course}</div>
+                          </div>
+                        )
+                      })}
+                    </div>
+                  </CardContent>
                 </Card>
               </div>
             </TabsContent>
--- components/ui/icons.jsx
+++ components/ui/icons.jsx
@@ -137,4 +137,20 @@ export const Icons = {
       <path d="M21 12a9 9 0 1 1-6.219-8.56" />
     </svg>
   ),
+  pagespinner: (props) => (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="1"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      {...props}
+    >
+      <path d="M21 12a9 9 0 1 1-6.219-8.56" />
+    </svg>
+  ),
 }
