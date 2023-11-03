7cf20b028934ccaa2aac4f7d8e44cebfea832fce
Alexander <alexander.l.lai@student.unsw.edu.au>
2023-11-03 18:20:56 +1100

    feat: Serverside to appearance settings

--- app/settings/page.jsx
+++ app/settings/page.jsx
@@ -1,6 +1,15 @@
+"use client"
+import { useEffect, useState } from "react";
 import { ProfileSettingsForm } from "../../components/forms/ProfileSettingsForm";
+import { getLanguages } from "@/api/getLanguages";
 
 export default function SettingsPage() {
+
+  const [languages, setLanguages] = useState([]);
+
+  useEffect(async () => {
+    setLanguages(await getLanguages())
+  });
   return (
     <div className="flex flex-col">
       <ProfileSettingsForm />
