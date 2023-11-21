1e35e1f4a6a7de0585629396ac471db518e6d083
Alexander <alexander.l.lai@student.unsw.edu.au>
2023-11-21 21:08:03 +1100

    feat: We can now view changes as soon as you add in a file Also have loading icon when awaiting for file to upload

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -32,6 +32,7 @@ import { getUser, getUserDetails, getConnections, addConnections, removeConnecti
 import { Button } from '@/components/ui/button';
 import { useSelector } from 'react-redux';
 import { Icons } from '@/components/ui/icons';
+import { convertToBase64 } from '@/lib/utils';
 
 export default function ProfilePage() {
   const searchParams = useSearchParams();
@@ -45,6 +46,8 @@ export default function ProfilePage() {
   const [initials, setInitials] = useState("");
   const [loading, setLoading] = useState(true);
   const [isLoading, setIsLoading] = useState(false);
+  const [profileLoad, setProfileLoad] = useState(false);
+  const [profilePic, setProfilePic] = useState({ myFile: "" })
 
   const userAuth = useSelector((state) => state.authenticationState.value);
   const searcherEmail = userAuth.email;
@@ -105,7 +108,17 @@ export default function ProfilePage() {
 
   const handleNewPic = async (e) => {
     e.preventDefault();
-    await addProfilePic(e.target.value);
+
+    const img = e.target.files[0];
+    if (!img) return;
+    
+    const base64 = await convertToBase64(img);
+    setProfilePic({...profilePic, myFile: base64});
+    setProfileLoad(true);
+    // await addProfilePic(e.target.value);
+    setTimeout(function () {
+      setProfileLoad(false);
+    }, 2000)
   }
 
   return (
@@ -116,23 +129,38 @@ export default function ProfilePage() {
           <div className="flex flex-col w-[60%] pt-6">
             <Card className=" bg-secondary w-full">
               <CardHeader className="items-center md:items-start">
-                <label htmlFor="file-upload" className="relative group overflow-hidden h-[150px] w-[150px] bg-border rounded-full sm:h-[200px] sm:w-[200px]">
-                  <div className="hidden text-white group-hover:block absolute top-[42%] left-[15%] sm:top-[45%] sm:left-[23%]">Upload Image</div>
-                  <div className=" group-hover:opacity-40 h-full w-full">
-                    <Avatar>
-                      <AvatarImage alt="profile" src="./assets/user.png" className="w-full h-full"/>
-                      <AvatarFallback className="bg-primary text-primary-foreground">{initials}</AvatarFallback>
-                    </Avatar>
-                  </div>
-                </label>
-                <input
-                  type="file"
-                  label="Image"
-                  id="file-upload"
-                  accept=".jpeg, .png, .jpg"
-                  className="hidden"
-                  onChange={handleNewPic}
-                />
+                {
+                  searcherEmail === email ?
+                    <>
+                      <label htmlFor="file-upload" className="relative group overflow-hidden h-[150px] w-[150px] bg-border rounded-full sm:h-[200px] sm:w-[200px]">
+                        <div className="hidden text-white group-hover:block absolute top-[42%] left-[15%] sm:top-[45%] sm:left-[23%]">Upload Image</div>
+                        <div className=" group-hover:opacity-40 h-full w-full cursor-pointer">
+                          {
+                            profileLoad ?
+                            <Icons.spinner className=" text-primary mr-2 h-full w-full animate-spin" /> :
+                            <Avatar>
+                              <AvatarImage alt="profile" src={profilePic.myFile || "./assets/user.png"} className="w-full h-full"/>
+                              <AvatarFallback className="bg-primary text-primary-foreground">{initials}</AvatarFallback>
+                            </Avatar>
+                          }
+                        </div>
+                      </label>
+                      <input
+                        type="file"
+                        label="Image"
+                        id="file-upload"
+                        accept=".jpeg, .png, .jpg"
+                        className="hidden"
+                        onChange={handleNewPic}
+                      />
+                    </> :
+                    <div className="relative group overflow-hidden h-[150px] w-[150px] bg-border rounded-full sm:h-[200px] sm:w-[200px]">
+                      <Avatar>
+                        <AvatarImage alt="profile" src={profilePic.myFile || "./assets/user.png"} className="w-full h-full"/>
+                        <AvatarFallback className="bg-primary text-primary-foreground">{initials}</AvatarFallback>
+                      </Avatar>
+                    </div>
+                }
               </CardHeader>
               <CardContent>
                 <div className="flex flex-col space-y-2 sm:flex-row sm:space-y-0 sm:space-x-6 justify-between items-center">
